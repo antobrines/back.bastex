059b5a1e533ed25eb59c39f3c22e44daa379c31c
antobrines <antonin.brines@outlook.fr>
2023-08-07 17:46:44 +0200

    fix: 🐛 config

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-const config = require('../config/config');
+const config = require('../config');
 const passport = require('passport');
 
 router.get('/discord', passport.authenticate('discord'), (req, res) => {});
